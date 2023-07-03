c21f4cef5981d12d704dd9eea07503e31cb646eb
Nerius Perez Toirac (Globant) <neriusperez.globant@latam.com>
2023-07-03 03:19:25 -0300

    Add init develop files

---
+++ public_html/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NXBX1EE587"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NXBX1EE587');
+</script>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<script src="js/jquery-2.2.4.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Moa Tienda Online</title>
+</head>
+<body>
+
+<header>
+    <h1>Moa Tienda Online</h1>	
+<p>Si quieres colaborar deja tu PR en nuestro repositorio:</p>
+<a href="https://github.com/moatiendaonline/moatiendaonline.git" target="_blank">https://github.com/moatiendaonline/moatiendaonline.git</a>   
+    <p>El propósito de esta página, es que como comunidad en #Moa #Cuba, podamos crear una Tienda Online, que nos permita como ciudadanos libres, intercambiar nuestros productos y servicios, y depender lo menos que se pueda del régimen actual imperante en la Isla.
+			
+</p>
+
+<p>Como desarrolladores, en diferentes espacios de las redes sociales, estaremos incorporando ideas, código fuentes que hagan crecer esta página al servicio de la comunidad.
+			</p>
+
+<p>Nuestros valores y principios: Respecto a la libertad, la propiedad individual-privada.
+No estamos de acuerdo con el #Comunismo, #Socialismo, debido al daño que ha producido a nuestra sociedad en #Moa y #Cuba.
+			</p>
+
+<p>
+Siguientes pasos:
+- Programar módulo de personas.
+- Programar módulo de productos y servicios.
+</p>
+
+<p>
+¡Por donaciones!: Transferir a la billetera crypto moneda trx(Tron) a esta dirección: TLvYarJnEUbxLNAJHDSbqnwupfp8ZA8dqL
+			</p>
+	    	</div>
+	    </div>
+    </div>
+</header>
+</body>
+</html>
